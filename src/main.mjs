5e519e240cc9b407f6dc0c9f59cd38c2cf5da6a7
Jose Pedro Dias <jose.pedro.dias@gmail.com>
2021-04-03 14:34:55 +0100

    refactor track parsing as a dedicated module

--- src/main.mjs
+++ src/main.mjs
@@ -1,83 +1,35 @@
 import { loadSprites, drawPolygon, drawRect, drawText, drawSprite, runGameLoop } from './canvas.mjs';
 import { subscribeKeys, keysDown } from './kbd.mjs';
-import { projectFactory } from './gis.mjs';
-import { limits2, parametric, movePolar, clamp, wrapAngle, mulVScalar, RAD2DEG } from './math.mjs';
+import { movePolar, clamp, wrapAngle, mulVScalar, RAD2DEG } from './math.mjs';
 import { drawCircle } from './canvas.mjs';
+import { parseTrack } from './parseTrack.mjs';
 
 async function run() {
-    //const res = await fetch('./assets/maps/portimao.geojson');
-    const res = await fetch('./assets/maps/portimao.2d.rt.geojson');
-    const data = await res.json();
-
-    // https://www.openstreetmap.org/#map=16/37.2303/-8.6279
-
     const WINDOW_SIZE = 800;
     const ZOOM = 16;
-    const ZOOM_TO_METERS = 0.015; // has not been calculated, estimated by trial and error
     const FONT_SIZE = 10;
     const CAR_SPRITE_ZOOM = 1 / 15;
+    const SPRITE_DIMS = [83, 34];
+    const SPRITE_NAMES = '01 02 03 04 05 06 07 08 09 10'.split(' ');
+
     const ctx = document.querySelector('canvas').getContext('2d');
     ctx.font = `${FONT_SIZE}px sans-serif`;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = '#330';
 
-    const pr = projectFactory(ZOOM);
+    const data = await parseTrack('./assets/maps/portimao.2d.rt.geojson', { zoom: ZOOM });
+    //console.log('data', data);
+    //window.data = data;
 
-    // will store right and left margins for both track and pit
-    const trackMargins = new Array(2);
-    const pitMargins = new Array(2);
-
-    // we ought to visit the track first to find its boundaries and center the canvas there...
-    const trackFeature = data.features.find(f => f.geometry?.type === 'LineString' && f?.properties['rt:kind'] === 'track');
-    if (!trackFeature) { throw new Error('Missing a LineString with rt:kind defined astrack'); }
-
-    let ox, oy;
-    {
-        const geo = trackFeature.geometry;
-        const coords = geo.coordinates;
-        const coords2 = coords.map(pr);
-        const limits = limits2(coords2);
-        const w = limits[0][1] - limits[0][0];
-        const h = limits[1][1] - limits[1][0];
-        [ox, oy] = [limits[0][0] - (0 - w) / 2, limits[1][0] - (0 - h) / 2];
-        //[ox, oy] = [limits[0][0], limits[1][0]];
+    function drawTrack() {
+        drawPolygon(ctx, data.track.left, { close: true });
+        drawPolygon(ctx, data.track.right, { close: true });
+        drawPolygon(ctx, data.pit.left);
+        drawPolygon(ctx, data.pit.right);
     }
+    //drawTrack();
 
-    data.features.forEach(feature => {
-        const geo = feature.geometry;
-        const props = feature.properties;
-        const kind = props['rt:kind'];
-        //console.log(props);
-
-        if (geo.type === 'LineString') {
-            const coords = geo.coordinates;
-            const coords2 = coords.map(pr);
-            const coords3 = coords2.map(([x, y]) => [x - ox, y - oy]);
-
-            if (['track', 'pit'].includes(kind)) {
-                let marginsArray = kind === 'track' ? trackMargins : pitMargins;
-
-                const width = parseFloat(props["rt:width"]);
-                if (isNaN(width)) { throw new Error(`${kind} missing property rt:width!`) }
-                const w = width * ZOOM * ZOOM_TO_METERS;
-                const closed = false;
-                marginsArray[0] = parametric(coords3, Math.PI / 2, w, closed);
-                marginsArray[1] = parametric(coords3, -Math.PI / 2, w, closed);
-            } else {
-                // TODO
-                //drawPolygon(ctx, coords3);
-            }
-        }
-    });
-
-    drawPolygon(ctx, trackMargins[0], { close: true });
-    drawPolygon(ctx, trackMargins[1], { close: true });
-    drawPolygon(ctx, pitMargins[0]);
-    drawPolygon(ctx, pitMargins[1]);
-
-    const SPRITE_DIMS = [83, 34];
-    const SPRITE_NAMES = '01 02 03'.split(' ');
     const sprites = await loadSprites(SPRITE_NAMES, 'assets/cars/{spriteName}.png');
 
     subscribeKeys();
@@ -117,10 +69,7 @@ async function run() {
         ctx.translate(cam_center[0], cam_center[1]);
         ctx.scale(cam_zoom, cam_zoom);
 
-        drawPolygon(ctx, trackMargins[0], { close: true });
-        drawPolygon(ctx, trackMargins[1], { close: true });
-        drawPolygon(ctx, pitMargins[0]);
-        drawPolygon(ctx, pitMargins[1]);
+        drawTrack();
 
         //drawCircle(ctx, car_pos, SPRITE_DIMS[1]);
 
@@ -134,7 +83,7 @@ async function run() {
         //ctx.scale(zoom, zoom);
 
         drawCircle(ctx, car_pos, SPRITE_DIMS[1]);
-        drawSprite(ctx, sprites[0], SPRITE_DIMS, car_pos, car_angle);
+        drawSprite(ctx, sprites[8], SPRITE_DIMS, car_pos, car_angle);
         ctx.restore();
 
         if (keysDown['ArrowUp']) {
@@ -174,8 +123,6 @@ async function run() {
         if (keysDown['v']) {
             cam_angle = wrapAngle(cam_angle - dt * 1);
         }
-
-        //if (keysJustDown[ARROW_UP]) { console.log('UP JUST GOT PRESSED'); }
 
         if (car_speed !== 0) {
             car_pos = movePolar(car_pos, car_angle, car_speed * dt);
---
+++ src/parseTrack.mjs
@@ -0,0 +1,90 @@
+/*
+./assets/maps/portimao.2d.rt.geojson
+https://www.openstreetmap.org/#map=16/37.2303/-8.6279
+*/
+import { projectFactory } from './gis.mjs';
+import { limits2, parametric } from './math.mjs';
+
+const ZOOM_TO_METERS = 0.015; // has not been calculated, estimated by trial and error
+
+const GJ_GEO_TYPE_LS = 'LineString';
+
+const RT_KIND = 'rt:kind';
+const RT_WIDTH = 'rt:width';
+
+const RT_KIND_TRACK = 'track';
+const RT_KIND_PIT = 'pit';
+
+export async function parseTrack(url, { zoom } = {}) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const pr = projectFactory(zoom);
+
+    // we ought to visit the track first to find its boundaries and center the canvas there...
+    const trackFeature = data.features.find(f => f.geometry?.type === GJ_GEO_TYPE_LS && f?.properties[RT_KIND] === RT_KIND_TRACK);
+    if (!trackFeature) { throw new Error('Missing a LineString with rt:kind defined as track'); }
+
+    const api = {
+        fromMeters(dim) {
+            return dim * zoom * ZOOM_TO_METERS;
+        }
+    }
+    const output = {
+        track: {}, // poly arrays (left, center and right)
+        pit: {}, // poly arrays (left, center and right)
+        dimensions: [], // [w, h]
+        center: [] // [x, y]
+    }
+
+    let ox, oy;
+    {
+        const geo = trackFeature.geometry;
+        const coords = geo.coordinates;
+        const coords2 = coords.map(pr);
+        const limits = limits2(coords2);
+        const width = limits[0][1] - limits[0][0];
+        const height = limits[1][1] - limits[1][0];
+        output.dimensions = [width, height];
+        [ox, oy] = [limits[0][0] - (0 - width) / 2, limits[1][0] - (0 - height) / 2];
+        //[ox, oy] = [limits[0][0], limits[1][0]];
+        output.center = [ox, oy];
+    }
+
+    data.features.forEach(feature => {
+        const geo = feature.geometry;
+        const props = feature.properties;
+        const kind = props[RT_KIND];
+        //console.log(props);
+
+        if (geo.type === GJ_GEO_TYPE_LS) {
+            const coords = geo.coordinates;
+            const coords2 = coords.map(pr);
+            const coords3 = coords2.map(([x, y]) => [x - ox, y - oy]);
+
+            if ([RT_KIND_TRACK, RT_KIND_PIT].includes(kind)) {
+                let bag = kind === RT_KIND_TRACK ? output.track : output.pit;
+
+                props[RT_WIDTH] = parseFloat(props[RT_WIDTH]);
+
+                const width = props[RT_WIDTH];
+                if (isNaN(width)) { throw new Error(`${kind} missing property rt:width!`) }
+                const w = api.fromMeters(width);
+                const closed = false;
+
+                bag.properties = props;
+                bag.left = parametric(coords3, -Math.PI / 2, w, closed)
+                bag.center = coords3;
+                bag.right = parametric(coords3, Math.PI / 2, w, closed);
+            } else {
+                // TODO
+                //drawPolygon(ctx, coords3);
+            }
+        }
+    });
+
+    return {
+        ...output,
+        ...api
+    }
+}
